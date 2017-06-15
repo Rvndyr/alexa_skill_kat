b4d42c56d9cf79c445b878a279db7f4893f9a065
randyr2125 <Randyr2125@msn.com>
2017-06-15 18:57:24 -0400

    added actions file with api key

--- src/NewsAPI.js
+++ src/NewsAPI.js
@@ -2,15 +2,23 @@ const axios = require('../node_modules/axios/index')
 const {
     newsApiKey,
 } = require('./config');
-const URL_BASE = 'https://newsapi.org/v1/sources?language=en';
 
-const paramify = (params) => Object.keys(params).reduce((_arr, key) => _arr.concat([[key, params[key]].join('=')]), [
-    ['key', newsApiKey].join('='),
-]).join('&');
-
-const GET = (endpoint, params) => {
-    return axios.get(URL_BASE + endpoint + '?' + paramify(params));
-}
+axios.get('https://newsapi.org/v1/sources?language=en')
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+// const URL_BASE = 'https://newsapi.org/v1/sources?language=en';
+//
+// const paramify = (params) => Object.keys(params).reduce((_arr, key) => _arr.concat([[key, params[key]].join('=')]), [
+//     ['key', newsApiKey].join('='),
+// ]).join('&');
+//
+// const GET = (endpoint, params) => {
+//     return axios.get(URL_BASE + endpoint + '?' + paramify(params));
+// }
 
 module.exports = {
     GET,
--- src/responses.js
+++ src/responses.js
@@ -1,3 +1,9 @@
+const {
+  getArticles,
+} = require('./actions');
+
+
+
 const initSessionAttributes = () => {
     // If we wanted to initialize the session to have some attributes we could add those here.
     const sessionAttributes = {};
@@ -6,11 +12,12 @@ const initSessionAttributes = () => {
 }
 
 const getWelcomeResponse = () => {
-    const cardTitle = 'Welcome';
-    const speechOutput = 'Welcome to Alexa Bot. ';
+    const cardTitle = 'Whats New?';
+    const speechOutput = 'Welcome to read about it, You can ask me for updates on articles. ';
+    // only giving updates on the newsAPI for now -- 6/13/17
     // If the user either does not reply to the welcome message or says something that is not
     // understood, they will be prompted again with this text.
-    const repromptText = 'Ask me about x y and z ';
+    const repromptText = 'Ask me about New articles';
     const shouldEndSession = false;
 
     return Promise.resolve()
@@ -21,6 +28,23 @@ const getWelcomeResponse = () => {
             shouldEndSession
         ]);
 }
+
+
+const getNewsArticles = () => getArticles().then(articles => {
+  const cardTitle = 'News Updates';
+  const speechOutput = 'Here are the latest articles ' + articles;
+
+  const repromptText = 'Would you like to continue hearing the latest articles?'
+  const shouldEndSession = false;
+
+  return [
+    cardTitle,
+    speechOutput,
+    repromptText,
+    shouldEndSession
+  ];
+})
+
 
 const getEndResponse = () => {
     const cardTitle = 'Conversation completed.';
@@ -41,8 +65,10 @@ const intentRequest = (intentRequest, session) => {
     const {name: intentName} = intent;
 
     switch (intentName) {
-        case 'SomeRequestIntent':
-            return /* implement your own response method here */;
+        case 'newsArticlesIntent':
+            return  getNewsArticles(); /* this will get you the news article sources API call */;
+        case ''
+            return
         case 'AMAZON.StopIntent':
         case 'AMAZON.CancelIntent':
             return getEndResponse();
